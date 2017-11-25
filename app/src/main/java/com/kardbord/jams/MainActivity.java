36ebfa51b198d1fa675a386de04fd337ad871469
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-25 13:42:45 -0700

    Added ArtistFragment

--- app/src/main/java/com/kardbord/jams/MainActivity.java
+++ app/src/main/java/com/kardbord/jams/MainActivity.java
@@ -3,35 +3,36 @@ package com.kardbord.jams;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFrag = null;
             switch (item.getItemId()) {
                 case R.id.navigation_artists:
-                    mTextMessage.setText(R.string.artists);
-                    return true;
+                    selectedFrag = new ArtistFragment();
+                    break;
                 case R.id.navigation_albums:
-                    mTextMessage.setText(R.string.albums);
-                    return true;
+                    break;
                 case R.id.navigation_songs:
-                    mTextMessage.setText(R.string.songs);
-                    return true;
+                    break;
                 case R.id.navigation_playlists:
-                    mTextMessage.setText(R.string.playlists);
-                    return true;
+                    break;
                 default:
                     return false;
             }
+            if (selectedFrag != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFrag).commit();
+            }
+            return true;
         }
 
     };
@@ -41,9 +42,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Manually set first fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new ArtistFragment()).commit();
     }
 
 }
