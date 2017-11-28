7a1562e34b11dd5e24f606a8c3bd34c936c3f276
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-28 10:08:39 -0700

    Added and populated ArrayList<Audio> member in PlaylistFragment. Also added a ListView member, but haven't populated. Need a way to create and track playlists

--- app/src/main/java/com/kardbord/jams/PlaylistFragment.java
+++ app/src/main/java/com/kardbord/jams/PlaylistFragment.java
@@ -1,17 +1,28 @@
 package com.kardbord.jams;
 
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PlaylistFragment extends Fragment {
+
+    private ArrayList<Audio> m_audioList;
+
+    private ListView m_listView;
 
 
     public PlaylistFragment() {
@@ -22,8 +33,35 @@ public class PlaylistFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlist, container, false);
+        View v = inflater.inflate(R.layout.fragment_playlist, container, false);
+        m_listView = v.findViewById(R.id.playlistList);
+
+        loadAudio();
+
+        return v;
+    }
+
+    // TODO: implement playlist somehow...
+
+    private void loadAudio() {
+        ContentResolver contentResolver = getActivity().getContentResolver();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
+        String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
+        Cursor cursor = contentResolver.query(uri, null, selection, null, sortOrder);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            m_audioList = new ArrayList<>();
+            while (cursor.moveToNext()) {
+                String data = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+                String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+                String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+                String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                m_audioList.add(new Audio(data, title, album, artist));
+            }
+        }
+        if (cursor != null) cursor.close();
     }
 
 }
