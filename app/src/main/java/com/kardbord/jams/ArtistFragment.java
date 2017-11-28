197391d6f83b3b90fc61a6bd21bc5bfc33a09846
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-27 20:09:07 -0700

    Added mListView to ArtistFragment

--- app/src/main/java/com/kardbord/jams/ArtistFragment.java
+++ app/src/main/java/com/kardbord/jams/ArtistFragment.java
@@ -2,10 +2,12 @@ package com.kardbord.jams;
 
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 /**
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class ArtistFragment extends Fragment {
 
+    private ListView mListView;
 
     public ArtistFragment() {
         // Required empty public constructor
@@ -22,8 +25,12 @@ public class ArtistFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_artist, container, false);
+        View v = inflater.inflate(R.layout.fragment_artist, container, false);
+        mListView = v.findViewById(R.id.mListView);
+
+
+        return v;
     }
+
 
 }
