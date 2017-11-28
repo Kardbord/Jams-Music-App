b646c1b891f05c44940ad33997e5ccf26000fc58
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-27 20:49:25 -0700

    Small victory! Got artists to display, albeit with many unknown artists, many duplicate artists, and not in alphabetical order

--- app/src/main/java/com/kardbord/jams/ArtistFragment.java
+++ app/src/main/java/com/kardbord/jams/ArtistFragment.java
@@ -1,13 +1,19 @@
 package com.kardbord.jams;
 
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,7 +21,9 @@ import android.widget.ListView;
  */
 public class ArtistFragment extends Fragment {
 
-    private ListView mListView;
+    private ArrayList<Audio> m_audioList;
+
+    private ListView m_listView;
 
     public ArtistFragment() {
         // Required empty public constructor
@@ -26,11 +34,43 @@ public class ArtistFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_artist, container, false);
-        mListView = v.findViewById(R.id.mListView);
+        m_listView = v.findViewById(R.id.mListView);
 
+        loadAudio();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, getArtists());
+        m_listView.setAdapter(adapter);
 
         return v;
     }
 
+    private ArrayList<String> getArtists() {
+        ArrayList<String> artists = new ArrayList<>();
+        for (Audio a : m_audioList) {
+            artists.add(a.getArtist());
+        }
+        return artists;
+    }
+
+    private void loadAudio() {
+        ContentResolver contentResolver = getActivity().getContentResolver();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
+        String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
+        Cursor cursor = contentResolver.query(uri, null, selection, null, sortOrder);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            m_audioList = new ArrayList<>();
+            while (cursor.moveToNext()) {
+                String data = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+                String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+                String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+                String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                m_audioList.add(new Audio(data, title, album, artist));
+            }
+        }
+        if (cursor != null) cursor.close();
+    }
 
 }
