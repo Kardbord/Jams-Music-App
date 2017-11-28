3cbee9f9f5ac7c4b2ad12bbd900ae6dddda18b10
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-28 10:19:07 -0700

    Got songs displaying properly

--- app/src/main/java/com/kardbord/jams/SongFragment.java
+++ app/src/main/java/com/kardbord/jams/SongFragment.java
@@ -1,11 +1,21 @@
 package com.kardbord.jams;
 
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
 
 
 /**
@@ -13,6 +23,9 @@ import android.view.ViewGroup;
  */
 public class SongFragment extends Fragment {
 
+    private ArrayList<Audio> m_audioList;
+
+    private ListView m_listView;
 
     public SongFragment() {
         // Required empty public constructor
@@ -22,8 +35,46 @@ public class SongFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_song, container, false);
+        View v = inflater.inflate(R.layout.fragment_song, container, false);
+        m_listView = v.findViewById(R.id.songList);
+
+        loadAudio();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_selectable_list_item, getSongs());
+        m_listView.setAdapter(adapter);
+
+        return v;
+    }
+
+    ArrayList<String> getSongs() {
+        Hashtable<String, String> hashedSongs = new Hashtable<>();
+        for (Audio a : m_audioList) {
+            if (!a.titleUnknown()) hashedSongs.put(a.getTitle(), a.getTitle());
+        }
+        ArrayList<String> songs = new ArrayList<>(hashedSongs.values());
+        Collections.sort(songs);
+        return songs;
+    }
+
+    private void loadAudio() {
+        ContentResolver contentResolver = getActivity().getContentResolver();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
+        String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
+        Cursor cursor = contentResolver.query(uri, null, selection, null, sortOrder);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            m_audioList = new ArrayList<>();
+            while (cursor.moveToNext()) {
+                String data = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+                String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+                String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+                String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                m_audioList.add(new Audio(data, title, album, artist));
+            }
+        }
+        if (cursor != null) cursor.close();
     }
 
 }
