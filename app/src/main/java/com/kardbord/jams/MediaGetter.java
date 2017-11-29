afaeb3c9e20761c1d9d47895e8899671adf71948
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-28 21:17:11 -0700

    added method to MediaGetter interface getPlaylists and implemented it in MainActivity

--- app/src/main/java/com/kardbord/jams/MediaGetter.java
+++ app/src/main/java/com/kardbord/jams/MediaGetter.java
@@ -2,7 +2,9 @@ package com.kardbord.jams;
 
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public interface MediaGetter {
     ArrayList<Audio> getAudioList();
+    Hashtable<String, String> getPlaylists();
 }
