9ee13f4f82eb71ab569db6a3d5a8c747cc9a1340
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-28 17:46:21 -0700

    Implemented parcelable for Audio class

--- app/src/main/java/com/kardbord/jams/Audio.java
+++ app/src/main/java/com/kardbord/jams/Audio.java
@@ -1,12 +1,14 @@
 package com.kardbord.jams;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.provider.MediaStore;
 
 import java.io.Serializable;
 import java.util.Objects;
 
 
-public class Audio implements Serializable {
+public class Audio implements Serializable, Parcelable {
 
     private String m_data;
     private String m_title;
@@ -19,6 +21,25 @@ public class Audio implements Serializable {
         this.m_album = album;
         this.m_artist = artist;
     }
+
+    protected Audio(Parcel in) {
+        m_data = in.readString();
+        m_title = in.readString();
+        m_album = in.readString();
+        m_artist = in.readString();
+    }
+
+    public static final Creator<Audio> CREATOR = new Creator<Audio>() {
+        @Override
+        public Audio createFromParcel(Parcel in) {
+            return new Audio(in);
+        }
+
+        @Override
+        public Audio[] newArray(int size) {
+            return new Audio[size];
+        }
+    };
 
     public boolean containsUnknown() {
         return (Objects.equals(m_artist, MediaStore.UNKNOWN_STRING) ||
@@ -59,4 +80,16 @@ public class Audio implements Serializable {
 
     public void setArtist(String artist) { m_artist = artist; }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(m_data);
+        parcel.writeString(m_title);
+        parcel.writeString(m_album);
+        parcel.writeString(m_artist);
+    }
 }
