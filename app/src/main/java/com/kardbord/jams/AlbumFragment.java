bf4a4893da28e6122009f8637c90fafee41c3d38
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-28 19:04:16 -0700

    AlbumFragment now utilizes mediaGetter to obtain audioList

--- app/src/main/java/com/kardbord/jams/AlbumFragment.java
+++ app/src/main/java/com/kardbord/jams/AlbumFragment.java
@@ -1,11 +1,8 @@
 package com.kardbord.jams;
 
 
-import android.content.ContentResolver;
-import android.database.Cursor;
-import android.net.Uri;
+import android.content.Context;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +20,8 @@ import java.util.Hashtable;
  */
 public class AlbumFragment extends Fragment {
 
+    private mediaGetter m_callback;
+
     private ArrayList<Audio> m_audioList;
 
     private ListView m_listView;
@@ -32,14 +31,20 @@ public class AlbumFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (!(context instanceof mediaGetter)) throw new AssertionError();
+        m_callback = (mediaGetter) context;
+        m_audioList = m_callback.getAudioList();
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_album, container, false);
         m_listView = v.findViewById(R.id.albumList);
-
-        loadAudio();
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_selectable_list_item, getAlbums());
 
@@ -56,27 +61,6 @@ public class AlbumFragment extends Fragment {
         ArrayList<String> albums = new ArrayList<>(hashedAlbums.values());
         Collections.sort(albums);
         return albums;
-    }
-
-    private void loadAudio() {
-        ContentResolver contentResolver = getActivity().getContentResolver();
-
-        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
-        String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
-        Cursor cursor = contentResolver.query(uri, null, selection, null, sortOrder);
-
-        if (cursor != null && cursor.getCount() > 0) {
-            m_audioList = new ArrayList<>();
-            while (cursor.moveToNext()) {
-                String data = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
-                String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
-                String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
-                String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
-                m_audioList.add(new Audio(data, title, album, artist));
-            }
-        }
-        if (cursor != null) cursor.close();
     }
 
 }
