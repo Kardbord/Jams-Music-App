b4892807121fa0120696df3b09344e1edda8db7b
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-12-04 19:57:53 -0700

    Implemented list item click navigation and back button to AlbumFragment

--- app/src/main/java/com/kardbord/jams/AlbumFragment.java
+++ app/src/main/java/com/kardbord/jams/AlbumFragment.java
@@ -7,12 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Objects;
 
 
 /**
@@ -22,6 +26,9 @@ public class AlbumFragment extends Fragment {
 
     private ArrayList<Audio> m_audioList;
 
+    private Button m_backButton;
+
+    private ListView m_listView;
 
     public AlbumFragment() {
         // Required empty public constructor
@@ -40,13 +47,22 @@ public class AlbumFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_album, container, false);
-        ListView m_listView = v.findViewById(R.id.albumList);
+        m_listView = v.findViewById(R.id.albumList);
+
+        m_backButton = v.findViewById(R.id.album_frag_back_button);
+        setButtonProperties(View.INVISIBLE, false);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_selectable_list_item, getAlbums());
 
         m_listView.setAdapter(adapter);
+        m_listView.setOnItemClickListener(albumClickListener);
 
         return v;
+    }
+
+    private void setButtonProperties(int visible, boolean clickable) {
+        m_backButton.setVisibility(visible);
+        m_backButton.setClickable(clickable);
     }
 
     private ArrayList<String> getAlbums() {
@@ -58,5 +74,37 @@ public class AlbumFragment extends Fragment {
         Collections.sort(albums);
         return albums;
     }
+
+    private ArrayList<String> getSongs(String album) {
+        HashSet<String> hashedSongs = new HashSet<>();
+        for (Audio a : m_audioList) {
+            if (!a.titleUnknown() && Objects.equals(a.getAlbum(), album)) hashedSongs.add(a.getTitle());
+        }
+        ArrayList<String> songs = new ArrayList<>(hashedSongs);
+        Collections.sort(songs);
+        return songs;
+    }
+
+    private View.OnClickListener backToAlbumOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_selectable_list_item, getAlbums());
+            m_listView.setAdapter(adapter);
+            setButtonProperties(View.INVISIBLE, false);
+            m_listView.setOnItemClickListener(albumClickListener);
+        }
+    };
+
+    private AdapterView.OnItemClickListener albumClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            setButtonProperties(View.VISIBLE, true);
+            String album = m_listView.getItemAtPosition(position).toString();
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_selectable_list_item, getSongs(album));
+            m_listView.setAdapter(adapter);
+            // TODO: set m_listView click listener to play the selected song
+            m_backButton.setOnClickListener(backToAlbumOnClick);
+        }
+    };
 
 }
