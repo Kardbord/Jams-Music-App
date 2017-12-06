ce94e1c0eed8fe096a8178bea79122a95bd1df21
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-12-06 15:41:34 -0700

    Completed step 3 of tutorial in MediaPlayerService class

--- app/src/main/java/com/kardbord/jams/MediaPlayerService.java
+++ app/src/main/java/com/kardbord/jams/MediaPlayerService.java
@@ -7,6 +7,9 @@ import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.IOException;
 
 // This class is based off of a tutorial found at
 // https://www.sitepoint.com/a-step-by-step-guide-to-building-an-android-audio-player-app/
@@ -16,7 +19,62 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
         AudioManager.OnAudioFocusChangeListener
 {
 
+    private MediaPlayer m_mediaPlayer;
+
+    // path to the audio file
+    private String m_mediaFile;
+
+    private int resumePosition;
+
     private IBinder iBinder = new LocalBinder();
+
+    private void initMediaPlayer() {
+        m_mediaPlayer = new MediaPlayer();
+        // Set up MediaPlayer event listeners
+        m_mediaPlayer.setOnCompletionListener(this);
+        m_mediaPlayer.setOnErrorListener(this);
+        m_mediaPlayer.setOnPreparedListener(this);
+        m_mediaPlayer.setOnBufferingUpdateListener(this);
+        m_mediaPlayer.setOnSeekCompleteListener(this);
+        m_mediaPlayer.setOnInfoListener(this);
+        // Reset so the MediaPlayer is not pointing to another data source
+        m_mediaPlayer.reset();
+
+        m_mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            m_mediaPlayer.setDataSource(m_mediaFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            stopSelf();
+        }
+    }
+
+    private void playMedia() {
+        if (!m_mediaPlayer.isPlaying()) {
+            m_mediaPlayer.start();
+        }
+    }
+
+    private void stopMedia() {
+        if (m_mediaPlayer == null) return;
+        if (m_mediaPlayer.isPlaying()) {
+            m_mediaPlayer.stop();
+        }
+    }
+
+    private void pauseMedia() {
+        if (m_mediaPlayer.isPlaying()) {
+            m_mediaPlayer.pause();
+            resumePosition = m_mediaPlayer.getCurrentPosition();
+        }
+    }
+
+    private void resumeMedia() {
+        if (!m_mediaPlayer.isPlaying()) {
+            m_mediaPlayer.seekTo(resumePosition);
+            m_mediaPlayer.start();
+        }
+    }
 
     @Nullable
     @Override
@@ -32,11 +90,27 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
     @Override
     public void onCompletion(MediaPlayer mp) {
         // Invoked when playback of a media source has completed
+        stopMedia();
+        // Stop the service
+        stopSelf();
     }
 
     @Override
     public boolean onError(MediaPlayer mp, int what, int extra) {
         // Invoked when there has been an error during an asynchronous operation
+        switch (what) {
+            case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
+                Log.d("MediaPlayer Error", "MEDIA ERROR NOT VALID FOR PROGRESSIVE PLAYBACK " + extra);
+                break;
+
+            case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
+                Log.d("MediaPlayer Error", "MEDIA ERROR SERVER DIED " + extra);
+                break;
+
+            case MediaPlayer.MEDIA_ERROR_UNKNOWN:
+                Log.d("MediaPlayer Error", "MEDIA ERROR UNKOWN " + extra);
+                break;
+        }
         return false;
     }
 
@@ -49,6 +123,7 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
     @Override
     public void onPrepared(MediaPlayer mp) {
         // Invoked when the media source is ready for playback
+        playMedia();
     }
 
     @Override
